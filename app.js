80a76a402444167956abad3faf1aeca6442d4093
Jennifer Santana <jennifersouzadesantana@gmail.com>
2024-03-14 08:27:50 -0300

    Configurei a pasta de arquivos estaticos.

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express')
+const path = require('path')
 const routes = require('./routes/router')
 const app = express()
 
-app.set('view engine')
+app.set('view engine', 'ejs')
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({extended:true}))
+
 
 app.use('/',routes)
 
 app.listen(3000,()=>{
-    console.log('http://localhost/3000')
+    console.log('http://localhost:3000')
 })
