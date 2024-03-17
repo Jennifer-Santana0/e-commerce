340c32c9f64663671ed9dea2f05a8bf838df6bb9
Jennifer Santana <jennifersouzadesantana@gmail.com>
2024-03-17 16:35:31 -0300

    Coloqeui o header na rota de produtos,reorganizei os arquivos na pasta de views.

--- controller/ProdutosControler.js
+++ controller/ProdutosControler.js
@@ -4,7 +4,7 @@ const showCategoria = async (req,res) => {
     const nome_produto = req.body.produto
     const produtos = await getProdut(nome_produto)
   
-    res.render('./components/produtos',{produtos})
+    res.render('./produtos',{produtos})
 }
 
 
