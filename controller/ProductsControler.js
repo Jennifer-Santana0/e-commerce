9613ff859e49ffbb55658c1935469e838f939b30
Jennifer Santana <jennifersouzadesantana@gmail.com>
2024-03-27 20:20:48 -0300

    Resolvi o problema de mostar a categoria de produtos.

--- controller/ProductsControler.js
+++ controller/ProductsControler.js
@@ -4,23 +4,24 @@ const getProdut = require('../api/api')
 const showCategory = async (req,res) => {
     let categoria = req.params.categoria
     let validacao_user = true
+    let id_user = req.params.id_user
 
     if(categoria == 'electronics'){
         const produtos = await getProdut(req.params.categoria)
-        res.render('./products', {validacao_user,produtos})
+        res.render('./products', {validacao_user,produtos,id_user})
     }else if (categoria == 'jewelery'){
         const produtos = await getProdut(req.params.categoria)
-        res.render('./products', {validacao_user,produtos})
+        res.render('./products', {validacao_user,produtos,id_user})
     }else if (categoria == "men's clothing"){
         const produtos = await getProdut(req.params.categoria)
-        res.render('./products', {validacao_user,produtos})
+        res.render('./products', {validacao_user,produtos,id_user})
     }else if (categoria == "women's clothing"){
         const produtos = await getProdut(req.params.categoria)
-        res.render('./products', {validacao_user,produtos})
+        res.render('./products', {validacao_user,produtos,id_user})
     } else {
         validacao_user = false
         let type_list = ['electronics','jewelery',"men's clothing","women's clothing"]
-        res.render('index', {type_list,validacao_user})
+        res.render('index', {type_list,validacao_user,id_user})
     }
     
 }
